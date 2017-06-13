7e6e9770fe81fa80cffb8fcafaedd06e7e75d702
Nguyễn Viết Cường <cuongdotvn@outlook.com>
2017-06-13 15:39:58 +0700

    V3

---
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        AppDbContext ctx = new AppDbContext();
+
+        [HttpPost]
+        public ActionResult Index(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                ctx.Employees.Add(emp);
+                ctx.SaveChanges();
+                return RedirectToAction("Message");
+            }
+            return View();
+        }
+
+        public ActionResult Message()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCDemo/Models/AppData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class AppData : DropCreateDatabaseIfModelChanges<AppDbContext>
+    {
+        protected override void Seed(AppDbContext context)
+        {
+            context.Employees.Add(new Employee()
+            {
+                Name = "ABC",
+                Designation = "Manager",
+                Department = "Sales"
+            });
+            base.Seed(context);
+        }
+    }
+}
